a19db72f9577a7d0eedae08d8de0b3428141f49d
John <jaboyak@gmail.com>
2018-02-11 03:45:04 -0700

    updated package.json and .storybook/config.js

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
 
-import 'babel-polyfill';
 import { configure } from '@storybook/react';
 
 function loadStories() {
