78ef3305c95488961996c3fa49ed81f61099913f
Justin <justhays124@gmail.com>
2019-09-16 11:11:16 -0600

    fixed error of missing class comp

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,24 +1,27 @@
 import React, { Component } from 'react'
 import { Switch } from '@material-ui/core'
 
-const ToDoItem = () => {
-  return (
-    <div className="collection-item">
-      <div className="row">
-        <div className="col s1">{ this.props.item.id }</div>
-        <div className="col s3">{ this.props.item.name }</div>
-        <div className="col s6">{ this.props.item.task }</div>
-        <div className="col s1">
-          <Switch  
-            value="checked" 
-            checked={ this.props.item.isComplete } 
-            onClick={ (e) => this.props.changeStatus(this.props.item.id, this.props.item.isComplete) } 
-            inputProps={ { 'aria-label': 'primary checkbox' } } 
-          />
+class ToDoItem extends Component {
+  render () {
+    return (
+      <div className="collection-item">
+        <div className="row">
+          <div className="col s1">{ this.props.item.id }</div>
+          <div className="col s3">{ this.props.item.name }</div>
+          <div className="col s6">{ this.props.item.task }</div>
+          <div className="col s1">
+            <Switch  
+              value="checked" 
+              checked={ this.props.item.isComplete } 
+              onClick={ (e) => this.props.changeStatus(this.props.item.id, this.props.item.isComplete) } 
+              inputProps={ { 'aria-label': 'primary checkbox' } } 
+            />
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
+ 
 }
 
 export default ToDoItem
